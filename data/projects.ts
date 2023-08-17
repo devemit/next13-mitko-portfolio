e5a3219d0597a6d34e0e30a4c33299959013477c
Mitko <mitkoiliev505@gmail.comm>
2023-08-17 02:18:52 +0200

    initial commit

---
+++ data/projects.ts
@@ -0,0 +1,64 @@
+export default [
+  {
+    title: 'Reform Beach&Bar',
+    imgUrl:
+      'https://user-images.githubusercontent.com/107273888/229608163-2a1aeed4-a133-4549-98cd-35f5abe72a35.png',
+    tech: ['Typescript', 'React', 'CSS'],
+    liveSite: 'https://reformbeachbar.vercel.app/',
+    seeCode: 'https://github.com/devemit/reform-menu',
+    description:
+      'Beach Bar Reform is a React web application designed to enhance the dining experience in restaurants by providing a seamless QR-scanning menu for customers. The application aims to reduce physical contact between customers and menus, making it more hygienic and convenient, especially in a post-pandemic world.',
+  },
+  {
+    title: 'E-Commerce Website',
+    imgUrl:
+    'https://user-images.githubusercontent.com/107273888/226775835-86d65498-a4f5-4216-8379-c348160de964.png',
+    tech: ['React', 'Tailwind CSS'],
+    liveSite: 'https://mitkos-shop-site.netlify.app',
+    seeCode: 'https://github.com/devemit/E-commerce-Website',
+    description:
+    'Ratio store is a  e-commerce platform build with React, a popular JavaScript library for building user interfaces. The goal of this project was to develop a modern and user-friendly online shopping experience that would allow customers to browse products, add items to their cart, and complete the checkout process seamlessly. ',
+  },
+  {
+    title: 'Cinemania/Netflix UI',
+    imgUrl:
+      'https://user-images.githubusercontent.com/107273888/217918647-c52b31f1-a7be-4edb-a1e2-d6eba1305442.png',
+    tech: ['Typescript', 'React', 'Tailwind CSS'],
+    liveSite: 'https://cinemaniamovs.netlify.app/',
+    seeCode: 'https://github.com/devemit/cinemania-movies',
+    description:
+      'In my recent project, I embarked on an exciting journey to create a Netflix clone using React, a powerful JavaScript library for building user interfaces. The primary objective of this project was to recreate the iconic Netflix streaming experience, offering users a platform to explore a vast library of movies and TV shows and enjoy their favorite content effortlessly.',
+  },
+
+  {
+    title: 'Crypto DEFI',
+    imgUrl:
+      'https://user-images.githubusercontent.com/107273888/218598621-557e03c0-f16e-42bc-aafa-d67373c9943d.png',
+    tech: ['Typescript', 'React', 'Tailwind CSS'],
+    liveSite: 'https://cryptowebdefi.netlify.app/',
+    seeCode: 'https://github.com/devemit/crypto-react',
+    description:
+      'In my recent project, I delved into the fascinating realm of cryptocurrencies by designing and developing a comprehensive website aimed at providing users with valuable insights, news, and resources related to the world of digital assets. This project was driven by the goal of creating a platform where both beginners and experienced enthusiasts can learn, stay updated, and engage with the rapidly evolving cryptocurrency landscape. ',
+  },
+
+  {
+    title: 'Portfolio Website',
+    imgUrl:
+      'https://user-images.githubusercontent.com/107273888/212379246-16fbfe8e-ae06-43ce-ac24-e884d7010e2e.png',
+    tech: ['React', 'Tailwind CSS'],
+    liveSite: 'https://mitko-portfolio.netlify.app/',
+    seeCode: 'https://github.com/devemit/Portfolio-Page',
+    description:
+      'This is my first personal portfolio which i used to show my skills and projects.',
+  },
+
+  {
+    title: 'Linktree Clone',
+    imgUrl:
+      'https://user-images.githubusercontent.com/107273888/212404531-60b20eaf-c810-4a91-a85e-f7347926cb9d.png',
+    tech: ['React', 'Tailwind CSS'],
+    liveSite: 'https://linktree-mitko.netlify.app/',
+    seeCode: 'https://github.com/devemit/linktree-clone',
+    description: 'Linktree Clone',
+  },
+];
---
+++ lib/pages.tsx
@@ -0,0 +1,5 @@
+export const pages = [
+  { label: 'home', href: '/', activeSegment: null },
+  { label: 'about', href: '/about', activeSegment: 'about' },
+  { label: 'projects', href: '/projects', activeSegment: 'projects' },
+];
